c5d487cf54a1ab6f02a61a789d583379e2d76db3
Jonathan Weber <bejonwe@gmx.net>
2015-07-23 10:32:57 +0200

    Initial code commit: Everything works, but it is definitely not the best solution and the design of the table could be better

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace moonshine_table_creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathPNGs = @".PNG\";
+            string fileExt = "*.png";
+
+            string[] files = Directory.GetFiles(pathPNGs, fileExt)
+                     .Select(path => Path.GetFileName(path))
+                     .ToArray();
+            string line = "| ";
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("list.txt"))
+            {
+                file.WriteLine("| Name          | Icon          | Name          | Icon          |");
+                file.WriteLine("| ------------- |:-------------:| ------------- |:-------------:|");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string[] name = files[i].Split('.');
+                    line += name[0] + " | [![" + name[0] + " icon](../../raw/master/.PNG/" + files[i] + ")](../../blob/master/.PNG/" + files[i] + ") | ";
+                    if (i != 0 && (i + 1) % 2 == 0)
+                    {
+                        file.WriteLine(line);
+                        line = "| ";
+                    }
+                }
+                file.WriteLine("Generated with the [Moonshine Table Creator](https://github.com/bejonwe/moonshine-table-creator)");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Moonshine Table Creator
+Generates a table for [PedroPegueroJr/moonshine-iconpack](https://github.com/PedroPegueroJr/moonshine-iconpack) with a preview of each icon and its name.
+## How to use
+Just copy the compiled binary to the cloned main folder of the [PedroPegueroJr/moonshine-iconpack](https://github.com/PedroPegueroJr/moonshine-iconpack) repository and run it. It will create a list.txt in the same directory, which contains a table in the "GitHub Flavored Markdown" format. This table contains all icons in the .PNG folder and links them to blob/master/.PNG/[ICONNAME].png .
+## License
+This software is licensed under the [GPL v2.0](https://www.gnu.org/licenses/gpl-2.0.html)
+## Special Thanks
+[PedroPegueroJr](https://github.com/PedroPegueroJr/moonshine-iconpack) - for the great application icon (which is part of the moonshine iconpack)!
